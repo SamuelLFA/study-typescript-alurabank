47c5dcbc1516e19c87e9c4d260c4bd362b054da9
Samuel <samuellfa3@gmail.com>
2021-05-16 11:33:14 -0300

    add decorators

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,20 +1,21 @@
+import { domInject } from '../helpers/decorators/domInject'
 import { Negociacao, Negociacoes } from '../models/index'
 import { MensagemView, NegociacoesView } from '../views/index'
 
 export class NegociacaoController {
 
+    @domInject('#data')
     private _inputData: JQuery<HTMLInputElement>
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery<HTMLInputElement>
+    @domInject('#valor')
     private _inputValor: JQuery<HTMLInputElement>
     private _negociacoes = new Negociacoes()
-    private _negociacoesView = new NegociacoesView('#negociacoesView')
-    private _mensagemView = new MensagemView('#mensagemView')
+    private _negociacoesView = new NegociacoesView('#negociacoesView', true)
+    private _mensagemView = new MensagemView('#mensagemView', true)
 
     constructor() {
 
-        this._inputData = $('#data')
-        this._inputQuantidade = $('#quantidade')
-        this._inputValor = $('#valor')
         this._negociacoesView.update(this._negociacoes)
     }
 
@@ -22,14 +23,36 @@ export class NegociacaoController {
 
         event.preventDefault()
 
+        let data = new Date((this._inputData.val() as string).replace(/-/g, '/'))
+
+        if (!this.ehDiaUtil(data)) {
+            this._mensagemView.update('Negociações somente em dias úteis')
+            return
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().toString()),
-            parseInt(this._inputQuantidade.val().toString()),
-            parseFloat(this._inputValor.val().toString())
+            data,
+            parseInt(this._inputQuantidade.val() as string),
+            parseFloat(this._inputValor.val() as string)
         )
 
         this._negociacoes.adiciona(negociacao)
         this._negociacoesView.update(this._negociacoes)
         this._mensagemView.update('Negociação adicionada com sucesso!')
     }
+
+    private ehDiaUtil(data: Date) {
+        return data.getDay() !== DiaDaSemana.Sabado || data.getDay() !== DiaDaSemana.Domingo
+    }
+}
+
+enum DiaDaSemana {
+
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
